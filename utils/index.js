7cc5010a71e56819dee340f0f7fc58e45c550c1d
lennert-mdware <hello@lennertderyck.be>
2021-04-13 23:01:35 +0200

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
 export const composeMail = ({ name, url }) => `
-    <h3>Dag ${ name }!</h3>
+    <h3>Dag ${ name },</h3>
     <p>
         Er staat een update van je project te wachten!<br>
         Bekijk het via <a href="${ url }" target="_blank" rel="noopener">${ url }</a>
